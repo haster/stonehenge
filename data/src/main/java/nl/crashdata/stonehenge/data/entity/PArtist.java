2f9d530449a18c8374e864a3fd0eb161859c08e6
hastes <sven.haster@kadaster.nl>
2019-02-23 13:42:10 +0100

    H2 database and entities now working

--- data/src/main/java/nl/crashdata/stonehenge/data/entity/PArtist.java
+++ data/src/main/java/nl/crashdata/stonehenge/data/entity/PArtist.java
@@ -9,6 +9,7 @@ public class PArtist {
     @Id
     @Column(name="id")
     @Access(AccessType.FIELD)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
     @Version
--- rest/src/main/java/nl/crashdata/stonehenge/rest/Application.java
+++ rest/src/main/java/nl/crashdata/stonehenge/rest/Application.java
@@ -3,11 +3,13 @@ package nl.crashdata.stonehenge.rest;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("nl.crashdata.stonehenge.data.repository")
+@EntityScan("nl.crashdata.stonehenge.data.entity")
 public class Application {
 
     public static void main(String[] args) {
