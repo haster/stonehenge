8f0a46269e5533effa8c7f3a0defcedfb8be5f14
hastes <sven.haster@kadaster.nl>
2019-02-23 16:01:57 +0100

    Fixed artist controller and prepared create method

--- rest/src/main/java/nl/crashdata/stonehenge/rest/controller/ArtistController.java
+++ rest/src/main/java/nl/crashdata/stonehenge/rest/controller/ArtistController.java
@@ -3,14 +3,21 @@ package nl.crashdata.stonehenge.rest.controller;
 import nl.crashdata.stonehenge.data.entity.PArtist;
 import nl.crashdata.stonehenge.data.repository.ArtistRepository;
 import nl.crashdata.stonehenge.rest.entity.Artist;
+import org.apache.catalina.connector.Response;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.persistence.EntityNotFoundException;
+import java.net.URI;
+import java.net.http.HttpResponse;
+import java.util.List;
 
-@RestController("/artist")
+@RestController
+@RequestMapping("/artist")
 public class ArtistController {
 
     @Autowired
@@ -24,14 +31,24 @@ public class ArtistController {
         return convertToRest(artistRepository.getOne(id));
     }
 
+    @PostMapping
+    public ResponseEntity<Artist> create(Artist artist) {
+        PArtist newArtist = artistRepository.save(convertToDb(artist));
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(newArtist.getId()).toUri();
+
+        return ResponseEntity.created(location).body(convertToRest(newArtist));
+    }
 
     private Artist convertToRest(PArtist artist) {
         return mapper.map(artist, Artist.class);
     }
 
+    private PArtist convertToDb(Artist artist) {
+        return mapper.map(artist, PArtist.class);
+    }
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(EntityNotFoundException.class)
-    public void handleEntityNotFound() {
-
-    }
+    public void handleEntityNotFound() {}
 }
