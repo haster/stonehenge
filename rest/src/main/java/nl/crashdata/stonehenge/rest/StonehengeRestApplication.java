28cd12708d6ff40e0b0b460d30c97b71c278c35d
Sven Haster <buurman.sven@gmail.com>
2019-08-16 10:18:01 +0200

    Setup a basic REST application

---
+++ rest/src/main/java/nl/crashdata/stonehenge/rest/StonehengeRestApplication.java
@@ -0,0 +1,10 @@
+package nl.crashdata.stonehenge.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/service")
+public class StonehengeRestApplication extends Application
+{
+
+}
